3fa5cb12bd0b0b84ae3636765fe96a36e87d5c10
Gyumin Kim <75244920+sinpodong@users.noreply.github.com>
2021-01-22 12:41:11 +0900

    1970/1/1 Standard Day Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -2,34 +2,62 @@ package calendar;
 
 public class Calendar {
 
-	private static final int[] MAX_DAY_ARR = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] LEAP_MAX_DAY_ARR = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LASTDAY_OF_MONTH_ARR = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LASTDAY_OF_MONTH_LEAP_ARR = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int DAY_OF_WEEK = 7;
 
 	public boolean isLeapYear(int year) {
-		if( year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
 		else
 			return false;
 	}
-	
-	public int getMaxDayOfMonth(int year, int month) {
-		if( isLeapYear(year))
-			return LEAP_MAX_DAY_ARR[month - 1];
+
+	public int getLastDayOfMonth(int year, int month) {
+		if (isLeapYear(year))
+			return LASTDAY_OF_MONTH_LEAP_ARR[month];
 		else
-			return MAX_DAY_ARR[month - 1];
+			return LASTDAY_OF_MONTH_ARR[month];
+	}
+
+	// STANDARD DAY = 1970 JAN 1st Thursday
+	public int getWeekDay(int year, int month, int day) {
+		int standard_year = 1970;
+		int standard_dayweek = 3; // 1970 JAN 1st => Thursday
+		int day_count = 0;
+
+		for (int i = standard_year; i < year; i++) {
+			day_count += isLeapYear(i) ? 366 : 365;
+		}
+
+		for (int i = 1; i < month; i++) {
+			day_count += getLastDayOfMonth(year, i);
+		}
+		
+		day_count += day;
+
+		return (day_count + standard_dayweek) % DAY_OF_WEEK;
 	}
 
 	public void printCalendar(int year, int month) {
 		System.out.printf("     <%dyear %dmonth>\n", year, month);
-		System.out.println("SUN MON TUE WEN THU FRI SAT");
+		System.out.println("SUN MON TUE WED THU FRI SAT");
 		System.out.println("---------------------------");
-		int lastdayOfMonth = getMaxDayOfMonth(year, month);
+		int lastdayOfMonth = getLastDayOfMonth(year, month);
+		int cur_dayWeek = getWeekDay(year, month, 1);
+
+		for (int i = 0; i < cur_dayWeek; i++) {
+			System.out.print("    ");
+		}
+
 		for (int i = 1; i <= lastdayOfMonth; i++) {
 			System.out.printf("%3d ", i);
 
-			if (i % DAY_OF_WEEK == 0) {
+			cur_dayWeek++;
+
+			if (cur_dayWeek % DAY_OF_WEEK == 0) {
 				System.out.println();
+				cur_dayWeek = 0;
 			}
 		}
 		System.out.println();
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -3,44 +3,44 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
+
 	private final static String PROMPT = "cal > ";
-	
+
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		int input_month = 1;
-		int input_year = -1;
+		int input_year = 1;
 
 		System.out.println("*****input the year and month*****");
-		System.out.println("program exit : month = -1");
-		while(input_month != -1) {
+		System.out.println("program exit : year = -1");
+		System.out.println("eX) 2021 1 / 2020 7");
+		while (input_year != -1) {
 			System.out.print(PROMPT);
-			
+
 			input_year = scanner.nextInt();
 			input_month = scanner.nextInt();
-			
-			if( input_month == -1 ) {
+
+			if (input_year == -1) {
 				System.out.println("Program exit!");
 				break;
 			}
-			
-			if( input_year <= 0 ) {
+
+			if (input_year <= 0) {
 				System.out.println("input the correct year!");
 				continue;
 			}
-			
-			if( input_month <= 0 || input_month > 12 ) {
+
+			if (input_month <= 0 || input_month > 12) {
 				System.out.println("input the correct month!");
 				continue;
 			}
-			
-//			System.out.printf("%d month has %d days.\n", input_month, cal.getMaxDayOfMonth(input_month));
+
 			cal.printCalendar(input_year, input_month);
 		}
-		
+
 		scanner.close();
-		
+
 	}
 
 }
