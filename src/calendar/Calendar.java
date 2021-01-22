0e104223b3cc5418c6efdd855b23b24d85ef1478
Gyumin Kim <75244920+sinpodong@users.noreply.github.com>
2021-01-22 15:58:47 +0900

    planer function add

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,10 +1,22 @@
 package calendar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Calendar {
 
 	private static final int[] LASTDAY_OF_MONTH_ARR = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LASTDAY_OF_MONTH_LEAP_ARR = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int DAY_OF_WEEK = 7;
+
+	private HashMap<Date, String> planMap;
+
+	public Calendar() {
+		planMap = new HashMap<Date, String>();
+	}
 
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
@@ -20,10 +32,10 @@ public class Calendar {
 			return LASTDAY_OF_MONTH_ARR[month];
 	}
 
-	// STANDARD DAY = 1970 JAN 1st Thursday
+	// STANDARD DAY = Thursday JAN 1st 1970
 	public int getWeekDay(int year, int month, int day) {
 		int standard_year = 1970;
-		int standard_dayweek = 3; // 1970 JAN 1st => Thursday
+		int standard_dayweek = 4; // 1970 JAN 1st => Thursday
 		int day_count = 0;
 
 		for (int i = standard_year; i < year; i++) {
@@ -33,8 +45,8 @@ public class Calendar {
 		for (int i = 1; i < month; i++) {
 			day_count += getLastDayOfMonth(year, i);
 		}
-		
-		day_count += day;
+
+		day_count += day - 1;
 
 		return (day_count + standard_dayweek) % DAY_OF_WEEK;
 	}
@@ -61,6 +73,27 @@ public class Calendar {
 			}
 		}
 		System.out.println();
+	}
+
+	public boolean addPlan(String Date, String Plan) {
+		try {
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(Date);
+			planMap.put(date, Plan);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public String searchPlan(String Date) {
+		try {
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(Date);
+			return planMap.get(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public static void main(String[] args) {
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -4,22 +4,89 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private final static String PROMPT = "cal > ";
+	private final static String PROMPT = "> ";
 
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		int input_month = 1;
-		int input_year = 1;
+		String in_cmd = "0";
 
+		printMenu();
+		while (!in_cmd.equals("q")) {
+			System.out.println("command (1, 2, 3, h, q)");
+			printPrompt();
+			in_cmd = scanner.next();
+
+			if (in_cmd.equals("1"))
+				prompt_addPlan(scanner, cal);
+			else if (in_cmd.equals("2"))
+				prompt_searchPlan(scanner, cal);
+			else if (in_cmd.equals("3"))
+				printCalendar(scanner, cal);
+			else if (in_cmd.equals("h"))
+				printHelpMsg();
+			else if (in_cmd.equals("q")) {
+				System.out.println("Bye!");
+				break;
+			} else {
+				System.out.println("input the correct command!");
+			}
+
+		}
+
+		scanner.close();
+
+	}
+
+	public void printPrompt() {
+		System.out.print(PROMPT);
+	}
+
+	public void prompt_addPlan(Scanner s, Calendar c) {
+		System.out.println("[Add Plan] input the date.");
+		printPrompt();
+		String in_date = s.next();
+
+		System.out.println("input the plan.");
+		printPrompt();
+		s.nextLine(); // ignore newline
+		String in_plan = s.nextLine();
+
+		if (c.addPlan(in_date, in_plan)) {
+			System.out.println("saved plan");
+		}
+	}
+
+	public void prompt_searchPlan(Scanner s, Calendar c) {
+		System.out.println("[Add Plan] input the date.");
+		printPrompt();
+		String in_date = s.next();
+
+		System.out.println(c.searchPlan(in_date));
+	}
+
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+	}
+
+	public void printCalendar(Scanner s, Calendar c) {
 		System.out.println("*****input the year and month*****");
 		System.out.println("program exit : year = -1");
 		System.out.println("eX) 2021 1 / 2020 7");
-		while (input_year != -1) {
-			System.out.print(PROMPT);
 
-			input_year = scanner.nextInt();
-			input_month = scanner.nextInt();
+		int input_month = 1;
+		int input_year = 1;
+
+		while (input_year != -1) {
+			printPrompt();
+
+			input_year = s.nextInt();
+			input_month = s.nextInt();
 
 			if (input_year == -1) {
 				System.out.println("Program exit!");
@@ -36,10 +103,11 @@ public class Prompt {
 				continue;
 			}
 
-			cal.printCalendar(input_year, input_month);
+			c.printCalendar(input_year, input_month);
 		}
+	}
 
-		scanner.close();
+	public void printHelpMsg() {
 
 	}
 
